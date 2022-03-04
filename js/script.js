bc68c6fa1fe28ba7a5affffb1fa9973f97b45b64
Nicholas-Kimutai <kimutainick74@gmail.com>
2022-03-05 00:37:55 +0300

    chore:Declare array Male

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var male = [
+    {day:"Sunday",akanName:"Kwasi"},
+    {day:"Monday",akanName:"Kwadwo"},
+    {day:"Tuesday",akanName:"Kwabena"},
+    {day:"Wednesday",akanName:"Kwaku"},
+    {day:"Thursday",akanName:"Yaw"},
+    {day:"Friday",akanName:"Kofi"},
+    {day:"Saturday",akanName:"Kwame"}
+     ];
+
