feff8d70ae0c1c5eaea4d5e045c9a5e2ffefef37
Nicholas-Kimutai <kimutainick74@gmail.com>
2022-03-05 19:42:53 +0300

    ft:add a slice function to split year into century and year digits

--- js/script.js
+++ js/script.js
@@ -8,28 +8,33 @@ var male = [
     {day:"Saturday",akanName:"Kwame"}
      ];
 
-     var female = [
-        {day:"Sunday",akanName:"Akosua"},
-        {day:"Monday",akanName:"Adwoa"},
-        {day:"Tuesday",akanName:"Abenaa"},
-        {day:"Wednesday",akanName:"Akua"},
-        {day:"Thursday",akanName:"Yaa"},
-        {day:"Friday",akanName:"Afua"},
-        {day:"Saturday",akanName:"Ama"}
+var female = [
+    {day:"Sunday",akanName:"Akosua"},
+    {day:"Monday",akanName:"Adwoa"},
+    {day:"Tuesday",akanName:"Abenaa"},
+    {day:"Wednesday",akanName:"Akua"},
+    {day:"Thursday",akanName:"Yaa"},
+    {day:"Friday",akanName:"Afua"},
+    {day:"Saturday",akanName:"Ama"}
     ];
     
     function dayOfWeek(){
-        var dd = parseInt(document.getElementById("dd").value);
-        var mm = parseInt(document.getElementById("mm").value);
-        var yy = document.getElementById("yy").value;
+        var dd = parseInt(document.getElementById("date").value);
+        var mm = parseInt(document.getElementById("month").value);
+        var year = document.getElementById("year").value;
+         
+        var cc=parseInt(year.slice(0,2));
+        var yy=parseInt(year.slice(2,4));
+        
 
-        var cc = parseInt(year.slice(0,2)); // century digit
-        var yy = parseInt(year.slice(2,4)); // year digit
+        if(dd <= 0 || dd > 31) {
+            alert("You have entered an invalid day");
+        }
+        if(mm <= 0 || mm > 12){
+            alert("You have entered an invalid month");
+        }
 
-        if(dd <= 0 || dd > 31) alert("You have entered an invalid day");
-        if(mm <= 0 || mm > 12) alert("You have entered an invalid month");
-
-        var day = (((cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd) % 7;
+        var day = (((cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd) % 7;;
         return Math.floor(day);
    
     };
@@ -49,12 +54,12 @@ var male = [
         var dayName = male[d].day;
 
         if(gender === "Female"){
-            document.querySelector("#akanName").innerHTML = "Your Akan Name is : " + femaleName;
-            document.querySelector("#day").innerHTML = "You were born on a : " + dayName;
+            document.querySelector("#akanNm").innerHTML = "Your Akan Name is : " + femaleName;
+            document.querySelector("#dayWeek").innerHTML = "You were born on a : " + dayName;
         }
         else if(gender==="Male"){
-            document.querySelector("#akanName").innerHTML = "Your Akan Name is : " + maleName;
-            document.querySelector("#day").innerHTML = "You were born on a : " + dayName;
+            document.querySelector("#akanNm").innerHTML = "Your Akan Name is : " + maleName;
+            document.querySelector("#dayWeek").innerHTML = "You were born on a : " + dayName;
         }
         else{
             alert("Invalid inputs");
