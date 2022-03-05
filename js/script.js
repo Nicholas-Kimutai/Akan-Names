161293d9ddbb9f02d6aa313b6dc7434620d581a8
Nicholas-Kimutai <kimutainick74@gmail.com>
2022-03-05 22:19:24 +0300

    st:add body background color

--- js/script.js
+++ js/script.js
@@ -19,9 +19,9 @@ var female = [
     ];
     
     function dayOfWeek(){
-        var dd = parseInt(document.getElementById("date").value);
-        var mm = parseInt(document.getElementById("month").value);
-        var year = document.getElementById("year").value;
+        var dd = parseInt(document.getElementById("dd").value);
+        var mm = parseInt(document.getElementById("mm").value);
+        var year = document.getElementById("yy").value;
          
         var cc=parseInt(year.slice(0,2));
         var yy=parseInt(year.slice(2,4));
@@ -32,6 +32,19 @@ var female = [
         }
         if(mm <= 0 || mm > 12){
             alert("You have entered an invalid month");
+        }
+
+
+        if(dd===null){
+            alert("Please enter the date of birth");
+        }
+
+        if(mm===null){
+            alert("Please enter the month of birth");
+        }
+
+        if(dd===null){
+            alert("Please enter the date year of birth");
         }
 
         var day = (((cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd) % 7;;
@@ -54,12 +67,12 @@ var female = [
         var dayName = male[d].day;
 
         if(gender === "Female"){
-            document.querySelector("#akanNm").innerHTML = "Your Akan Name is : " + femaleName;
-            document.querySelector("#dayWeek").innerHTML = "You were born on a : " + dayName;
+            document.getElementById("akanNm").innerHTML = "Your Akan Name is : " + femaleName;
+            document.getElementById("dayWeek").innerHTML = "You were born on a : " + dayName;
         }
         else if(gender==="Male"){
-            document.querySelector("#akanNm").innerHTML = "Your Akan Name is : " + maleName;
-            document.querySelector("#dayWeek").innerHTML = "You were born on a : " + dayName;
+            document.getElementById("akanNm").innerHTML = "Your Akan Name is: " + maleName;
+            document.getElementById("dayWeek").innerHTML = "You were born on a : " + dayName;
         }
         else{
             alert("Invalid inputs");
